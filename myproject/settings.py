2fad1b17ada8456b07a2e9053f90cc8c004b3274
THLaboratory <altogether175@gmail.com>
2025-01-29 09:42:25 +0900

    a

--- myproject/settings.py
+++ myproject/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = '/opt/render/project/src/staticfiles'
+STATIC_ROOT = os.getenv('STATIC_ROOT', '/opt/render/project/src/staticfiles')
 
 STATICFILES_DIRS = []
 
