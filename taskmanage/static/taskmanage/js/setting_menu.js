8e7fe2a05b4176cd5472736ee033f5807c41d93b
THLaboratory <altogether175@gmail.com>
2025-02-24 23:03:37 +0900

    Delete taskmanage/static/taskmanage directory

--- taskmanage/static/taskmanage/js/setting_menu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", manageAll);
-
-function manageAll() {
-    document.getElementById("menu-icon").addEventListener("click", () => {
-        const settingsPanel = document.getElementById("settings-panel");
-        settingsPanel.classList.toggle("active"); // activeクラスの付け外しで表示切替
-    });
-}
